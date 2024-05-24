37f1a373b30044677520fa8dcdf689eb063564b8
rafaelcontii <148920793+rafaelcontii@users.noreply.github.com>
2024-05-24 21:37:24 +0000

    inicio do trabalho .c

---
+++ trabalho.c
@@ -0,0 +1,9 @@
+#include "trabalho.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+Contato agenda_pessoal[MAX_CONTACTS];
+Contato agenda_trabalho[MAX_CONTACTS];
+int num_contatos_pessoais = 0;
+int num_contatos_trabalho = 0;
